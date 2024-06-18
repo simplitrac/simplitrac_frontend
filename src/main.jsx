3104c68fbcff357015e883068bdf7ce04e8bc564
David Brown <d.boyer.brown@gmail.com>
2024-06-18 15:53:12 -0400

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,7 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// import App from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  "base": "/simplitrac_frontend/",
   plugins: [react()],
-  base: "/SimpliTrac/"
 })
