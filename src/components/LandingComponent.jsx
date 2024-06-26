d559f56646334a8c7776637a70a9674b1b72acad
Patrick Snoop <patsnoop@gmail.com>
2024-06-25 23:00:34 -0400

    -m modify landing

--- src/components/LandingComponent.jsx
+++ src/components/LandingComponent.jsx
@@ -36,7 +36,6 @@ const LandingComponent = (props) => {
                     <Button variant="primary" onClick={() => renderNewScreen("landing")}>Expense</Button>
                     <Button variant="primary" onClick={() => renderNewScreen("camera")}>Camera</Button>
                     <Button variant="primary" onClick={() =>renderNewScreen("chart")}>Chart</Button>
-                    {/* <button onClick={logOut}> logOut</button> */}
                 </Col>
             </Row>
         </Container>
