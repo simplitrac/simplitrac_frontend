55a852efa3f354e92d6beb2bd528f9c702189844
Patrick Snoop <patsnoop@gmail.com>
2024-06-25 20:29:08 -0400

    -m fix

--- src/components/LandingComponent.jsx
+++ src/components/LandingComponent.jsx
@@ -36,7 +36,7 @@ const LandingComponent = (props) => {
                     <Button variant="primary" onClick={() => renderNewScreen("landing")}>Expense</Button>
                     <Button variant="primary" onClick={() => renderNewScreen("camera")}>Camera</Button>
                     <Button variant="primary" onClick={() =>renderNewScreen("chart")}>Chart</Button>
-                    <button onClick={logOut}> LogOut</button>
+                    {/* <button onClick={logOut}> logOut</button> */}
                 </Col>
             </Row>
         </Container>
