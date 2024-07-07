b35701eff340162352978f701c6fe8185128a23f
anpaulan <paulan21095@gmail.com>
2024-07-06 22:23:53 -0400

    user chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,70 +1,55 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-import BackButton from "./BackButton.jsx";
-import {useContext} from "react";
-import {AppContext} from "../context/AppContext.jsx";
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
-
-//
-// import React, { useEffect, useState } from 'react';
-//
-// // import './App.css';
-// import { auth, onAuthStateChanged } from '../config/initializeFirestore.js'
+// import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+// import BackButton from "./BackButton.jsx";
+// const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+// import React, {useEffect, useState} from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Container, Row, Col, Table, Button, Form, Modal } from 'react-bootstrap';
+// import './App.css';
 
 
+import React, { useEffect, useState } from 'react';
+import { auth, onAuthStateChanged } from "../config/initializeFirestore";
+import '../App.css';
+
+const Chart = () => {
+  const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserId(user.uid);
+      } else {
+        setUserId("");
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>SimpliTrac</h1>
+      {userId && <LookerStudioChart userId={userId} />}
+    </div>
+  );
+};
+
+const LookerStudioChart = ({ userId }) => {
+  const reportUrl = `https://lookerstudio.google.com/embed/reporting/fe5a072c-9afe-464c-9cef-2e04f351ff78/page/SVN5D?user_id=${userId}`;
+
+  return (
+    <div className="report-container">
+      <iframe 
+        id="report-frame" 
+        width="600" 
+        height="400" 
+        src={reportUrl} 
+        style={{ border: 0 }} 
+        allowFullScreen 
+      />
+    </div>
+  );
+};
+
+export default Chart;
 
 
-const Chart = () =>{
-
-    const {user} = useContext(AppContext)
-
-    const countCategories = () => {
-        const categories = user.categories
-
-        return categories.map((category) => {
-        return {
-            category: category,
-            count: user.transactions.filter((transaction) => transaction === category)}
-        })
-    }
-
-    return (
-        <>
-            <LineChart width={600} height={300} data={user}>
-                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                <CartesianGrid stroke="#ccc" />
-                <XAxis dataKey={user.categories} />
-                <YAxis dataKey={countCategories()}/>
-            </LineChart>
-            <BackButton />
-        </>
-    );
-}
-
-export default Chart
-
-//
-//
-// const Chart = () => {
-//     const [userId, setUserId] = useState("");
-//
-//     useEffect(() => {
-//         const unsubscribe = onAuthStateChanged(auth, (user) => {
-//             if (user) {
-//                 setUserId(user.uid);
-//             } else {
-//                 setUserId("");
-//             }
-//         });
-//         return () => unsubscribe();
-//     }, []);
-//
-//     return (
-//         <div className="App">
-//             <h1>SimpliTrac</h1>
-//             {userId && <LookerStudioChart userId={userId} />}
-//         </div>
-//     );
-// };
-//
-
-// export default Chart;
