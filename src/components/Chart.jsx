fbb0ed9166d7454046d35a09c0cc7563a7f4c7a9
anpaulan <paulan21095@gmail.com>
2024-07-09 13:47:23 -0400

    alt method to filter

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -5,18 +5,20 @@ import BackButton from "./BackButton.jsx";
 
 // Main Chart component
 const Chart = () => {
-  const [userId, setUserId] = useState(""); // State to store the user ID
+  const [userEmail, setUserEmail] = useState(""); // State to store the user email
 
   // useEffect hook to handle authentication state changes
   useEffect(() => {
     // Subscribe to authentication state changes
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
-        // Set user ID if user is logged in
-        setUserId(user.uid);
+        // Set user email if user is logged in
+        setUserEmail(user.email);
+        console.log(`User Email set: ${user.email}`);
       } else {
-        // Clear user ID if user is logged out
-        setUserId("");
+        // Clear user email if user is logged out
+        setUserEmail("");
+        console.log("No user is signed in");
       }
     });
     // Cleanup subscription on component unmount
@@ -26,24 +28,25 @@ const Chart = () => {
   return (
     <div className="App">
       <h1>SimpliTrac</h1>
-      {/* Display LookerStudioChart if user is logged in, otherwise prompt to log in */}
-      {userId ? <LookerStudioChart userId={userId} /> : <p>Please log in to see your data.</p>}
-      {/* Back button component */}
+      {userEmail ? <LookerStudioChart user_email={userEmail} /> : <p>Please Enter in an Expense.</p>}
       <BackButton />
     </div>
   );
 };
 
 // LookerStudioChart component to display embedded Looker Studio report
-const LookerStudioChart = ({ userId }) => {
-  // Construct report URL with user ID parameter
-  const reportUrl = `https://lookerstudio.google.com/embed/reporting/ae330055-31b8-4e65-a1a9-f0bbd1cda92f/page/87O5D?params={"userId":"${userId}"}`;
+const LookerStudioChart = ({ user_email }) => {
+  const reportUrl = `https://lookerstudio.google.com/embed/reporting/ae330055-31b8-4e65-a1a9-f0bbd1cda92f/page/87O5D?params=${encodeURIComponent(`{"user_email": "${user_email}","filter": {"user_email": "${user_email}"} }`)}`;
+
+  useEffect(() => {
+    console.log(`Report URL: ${reportUrl}`);
+  }, [reportUrl]);
 
   return (
     <div className="report-container">
       <iframe 
         id="report-frame" 
-        width="100%" 
+        width="900" 
         height="600" 
         src={reportUrl} 
         style={{ border: 0 }} 
