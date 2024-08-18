2ad3436e8f048f08d56803542e91bdf52b52555a
Kirn Kim <squrkidev@gmail.com>
2024-08-18 01:25:03 -0700

    Fix: adding processing spinner to category delete

--- src/components/LandingComponent.jsx
+++ src/components/LandingComponent.jsx
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import ExpensesTable from "./ExpensesTable.jsx";
 import CategoryModal from "./CategoryModal.jsx";
@@ -8,22 +8,22 @@ import OCRModal from "./OCRModal.jsx";
 import User from "../models/User.js";
 import ConfirmationModal from "./ConfirmationModal.jsx";
 import { AchievementProvider } from "react-achievements";
-import achievementConfig  from "../config/achievementConfig.js";
+import achievementConfig from "../config/achievementConfig.js";
 import '../App.css';
 import JoyrideTour from "./JoyRideTour.jsx";
 
 const LandingComponent = () => {
-    const { setScreen, ocrData, serverResponse, setServerResponse, user } = useContext(AppContext);
+    const { setScreen, ocrData, serverResponse, setServerResponse, user, setIsUpdating } = useContext(AppContext);
     const [showCategories, setShowCategories] = useState(false);
     const [runTour, setRunTour] = useState(false);
-    
+
     useEffect(() => {
         if (user.isNewUser && user.isNewUser()) {
             setRunTour(true);
         }
     }, [user]);
 
-    
+
     const renderNewScreen = (screen) => {
         if (screen === undefined) {
             return;
@@ -43,6 +43,7 @@ const LandingComponent = () => {
 
     const handleDeleteCategory = async (categoryId) => {
         if (window.confirm("Are you sure you want to delete this category?")) {
+            setIsUpdating(true)
             const updatedUser = new User(user);
             const result = await updatedUser.deleteCategory(categoryId);
 
@@ -53,47 +54,48 @@ const LandingComponent = () => {
                 setServerResponse('Failed to delete category');
             }
         }
+        setIsUpdating(false)
     }
 
     return (
-        <AchievementProvider  config={achievementConfig} initialState={user.serialize()} badgesButtonPosition={'top-right'}>
-                <Container fluid={true} className="landing-container">
-                    {< JoyrideTour run={runTour} setRun={setRunTour} />}
-                    {user.first_name && (
-                        <>
-                            <p>
-                                Welcome {user.first_name} {user.last_name}
-                            </p>
-                            {user.isNewUser() && <CategoryModal />}
-                            {ocrData && <OCRModal />}
-                            {serverResponse && <ConfirmationModal />}
-                        </>
-                    )}
-                    <div className="landing-header">
-                        <h1>SimpliTrac</h1>
-                    </div>
-                    <div className="landing-content">
-                        <ExpensesTable />
-                    </div>
-                    <div className="buttons-container">
-                        {/*<button className="custom-button" onClick={handleSubmit}>Submit</button>*/}
-                        <button className="custom-button" onClick={() => setRunTour(true)}>Start Tour</button>
-                        <button className="custom-button" onClick={() => renderNewScreen("camera")}>Camera</button>
-                        <button className="custom-button" onClick={() => renderNewScreen("chart")}>Chart</button>
-                        <button className="custom-button" onClick={() => renderNewScreen("edit")}>Edit Transactions</button>
-                        <button className="custom-button" onClick={() => renderNewScreen("userguide")}>User Guide</button>
-                        <button className="custom-button" onClick={(toggleCategoriesList)}>
-                            {showCategories ? "Hide Categories" : "Show Categories"}
-                        </button>
-                    </div>
-                    {showCategories && (
+        <AchievementProvider config={achievementConfig} initialState={user.serialize()} badgesButtonPosition={'top-right'}>
+            <Container fluid={true} className="landing-container">
+                {< JoyrideTour run={runTour} setRun={setRunTour} />}
+                {user.first_name && (
+                    <>
+                        <p>
+                            Welcome {user.first_name} {user.last_name}
+                        </p>
+                        {user.isNewUser() && <CategoryModal />}
+                        {ocrData && <OCRModal />}
+                        {serverResponse && <ConfirmationModal />}
+                    </>
+                )}
+                <div className="landing-header">
+                    <h1>SimpliTrac</h1>
+                </div>
+                <div className="landing-content">
+                    <ExpensesTable />
+                </div>
+                <div className="buttons-container">
+                    {/*<button className="custom-button" onClick={handleSubmit}>Submit</button>*/}
+                    <button className="custom-button" onClick={() => setRunTour(true)}>Start Tour</button>
+                    <button className="custom-button" onClick={() => renderNewScreen("camera")}>Camera</button>
+                    <button className="custom-button" onClick={() => renderNewScreen("chart")}>Chart</button>
+                    <button className="custom-button" onClick={() => renderNewScreen("edit")}>Edit Transactions</button>
+                    <button className="custom-button" onClick={() => renderNewScreen("userguide")}>User Guide</button>
+                    <button className="custom-button" onClick={(toggleCategoriesList)}>
+                        {showCategories ? "Hide Categories" : "Show Categories"}
+                    </button>
+                </div>
+                {showCategories && (
                     <div className="categories-list">
                         <h3>Your Categories</h3>
                         <ul className="category-list">
                             {user.categories.map((category) => (
                                 <li key={category.category_id} className="category-item">
                                     <span>{category.category_name}</span>
-                                    <button 
+                                    <button
                                         className="delete-button custom-button"
                                         onClick={() => handleDeleteCategory(category.category_id)}
                                     >
@@ -104,10 +106,10 @@ const LandingComponent = () => {
                         </ul>
                     </div>
                 )}
-                    <div className="landing-content">
-                        <SignOut />
-                    </div>
-                </Container>
+                <div className="landing-content">
+                    <SignOut />
+                </div>
+            </Container>
         </AchievementProvider>
     );
 };
