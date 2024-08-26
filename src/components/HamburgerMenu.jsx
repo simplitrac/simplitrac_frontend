15f67e9a030deef38a86052735fd7ec3559691b2
David Brown <d.boyer.brown@gmail.com>
2024-08-26 06:14:44 -0400

    update hamburger menu
    - update hamburger menu to Chakra
    - update Expenses table to Chakra

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,11 +1,30 @@
-import React, { useContext } from 'react';
-import { slide as Menu } from 'react-burger-menu';
+import React, { useContext, useEffect } from 'react';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  IconButton,
+  VStack,
+  useDisclosure,
+    Button
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import { AppContext } from '../context/AppContext';
 
 const HamburgerMenu = ({ setRunTour }) => {
-  const {showHamburger, setShowHamburger} = useContext(AppContext);
-  
-  
+  const { showHamburger, setShowHamburger } = useContext(AppContext);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  useEffect(() => {
+    if (showHamburger) {
+      onOpen();
+    } else {
+      onClose();
+    }
+  }, [showHamburger, onOpen, onClose]);
 
   const startTour = () => {
     setShowHamburger(false);
@@ -13,12 +32,40 @@ const HamburgerMenu = ({ setRunTour }) => {
   };
 
   return (
-    <Menu 
-    
-    >
-      <a onClick={startTour}>Start Tour</a>
-      {/* Add more menu items here */}
-    </Menu>
+      <>
+        <IconButton
+            aria-label="Open Menu"
+            icon={<HamburgerIcon />}
+            onClick={() => setShowHamburger(true)}
+            display={{ base: "block", md: "block" }}
+            position="fixed"
+            top="20px"
+            left="20px"
+            zIndex="1000"
+            colorScheme="teal"
+        />
+
+        <Drawer placement="left" onClose={() => setShowHamburger(false)} isOpen={isOpen}>
+          <DrawerOverlay>
+            <DrawerContent>
+              <DrawerCloseButton color='red'/>
+              <DrawerHeader>Menu</DrawerHeader>
+              <DrawerBody>
+                <VStack align="start" spacing={4}>
+                  <Button
+                      onClick={startTour}
+                      variant="ghost"
+                      w="100%"
+                      aria-label="Start Tour"
+                  >
+                    Start Tour
+                  </Button>
+                </VStack>
+              </DrawerBody>
+            </DrawerContent>
+          </DrawerOverlay>
+        </Drawer>
+      </>
   );
 };
 
